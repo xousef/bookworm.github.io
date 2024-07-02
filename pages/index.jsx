ed723b4181b7fb921fcebf197301a799fbcf4513
John king <30763634+xousef@users.noreply.github.com>
2024-07-02 00:21:16 -0700

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -26,8 +26,7 @@ export default function Home() {
             
                 </div>
                 <p className="text-white/50 text-md mt-3">
-I am a professional editor and software developer. I have been professionally interested in community management and development for 5 years, I have high level knowledge and experience in areas such as community management and social media management.
-                </p>
+Your site is published at... Github pages                </p>
               </div>
               <div className={`order-first lg:order-last flex-shrink-0 relative w-[160px] h-[160px] rounded-full pulse-avatar-online `}>
                 <img alt="umutbayraktar" src={`https://media.discordapp.net/attachments/737348411568685066/996841172372824184/YT_AVATAR_NET.jpg`} width="160" height="160" className={`bg-neutral-700 w-[160px] h-[160px] rounded-full`} />
